ec96fbe8b105aac253538e7aec119833508498e3
Erwan Keribin <erwan.keribin@gmail.com>
2018-07-05 16:04:55 +0200

    Add boostrap and crude basic design

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/bootstrap-4.1.1.min.css';
+import './css/app.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <div className="row text-center">
+          <div className="col-sm-12"><h1>ODE FrontManager</h1></div>
+        </div>
+        <div className="row text-center h-100 main">
+          <div className="col-sm-3 border rounded"><h1>Navbar</h1></div>
+          <div className="col-sm-9 border rounded"><h1>Data</h1></div>
+        </div>
       </div>
     );
   }
