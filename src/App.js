4740234fa300a65f3e9f1778002a12e978880427
Erwan Keribin <erwan.keribin@gmail.com>
2018-07-05 16:38:48 +0200

    Refactor Navbar into component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,20 @@ import React, { Component } from 'react';
 import './css/bootstrap-4.1.1.min.css';
 import './css/app.css';
 
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="col-sm-3 border rounded">
+        <h1>Navbar</h1>
+        <ul>
+          <li><a href="#">Datasets</a></li>
+          <li><a href="#">Annotation campaigns</a></li>
+        </ul>
+      </div>
+    )
+  }
+}
+
 class App extends Component {
   render() {
     return (
@@ -10,7 +24,7 @@ class App extends Component {
           <div className="col-sm-12"><h1>ODE FrontManager</h1></div>
         </div>
         <div className="row text-center h-100 main">
-          <div className="col-sm-3 border rounded"><h1>Navbar</h1></div>
+          <Navbar/>
           <div className="col-sm-9 border rounded"><h1>Data</h1></div>
         </div>
       </div>
